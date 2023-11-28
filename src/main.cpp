b8cda881022a70224542c43af4a03ac145a66a87
GaspardGentil <gaspard.gentil@gmail.com>
2023-11-28 18:50:59 +0100

    test failure

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char const *argv[])
 {
-    const char*file = "img/baboon.ascii.pgm";
+    const char*file = "/img/baboon.ascii.pgm";
     Graph test(file);
     test.printGraph(0,0);
     test.parcours(2,2);
